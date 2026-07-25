659d4a3b1d4faae6fcbb4fe48ec5713b52278948
Tim Keen <tim@keen.digital>
2026-07-25 05:59:35 -0400

    test: narrow workflow fake timers

--- packages/convex/test/workflow-conformance.test.ts
+++ packages/convex/test/workflow-conformance.test.ts
@@ -1506,7 +1506,7 @@ describe("Maestro V2 inline transaction compiler", () => {
 });
 
 describe("pinned Convex Workflow component conformance", () => {
-  beforeEach(() => vi.useFakeTimers());
+  beforeEach(() => vi.useFakeTimers({ toFake: ["Date", "setTimeout"] }));
   afterEach(() => {
     expect(vi.getTimerCount()).toBe(0);
     vi.useRealTimers();
