f13bc25f47177c39d5415a0bda23e7c55cd3cbf4
mini-headless <headless@modernagencysales.com>
2026-07-31 22:19:08 -0400

    feat(funnel): add verified report ownership capability

--- packages/convex/convex/capabilities/manageEvaluationReport.ts
+++ packages/convex/convex/capabilities/manageEvaluationReport.ts
@@ -1,6 +1,12 @@
 import registeredFunctions from "../../confect/_generated/registeredFunctions/capabilities/manageEvaluationReport";
 
+export const consumeReportEmailVerification =
+  registeredFunctions.consumeReportEmailVerification;
 export const getSharedEvaluationReport =
   registeredFunctions.getSharedEvaluationReport;
+export const listOwnedEvaluationReports =
+  registeredFunctions.listOwnedEvaluationReports;
 export const manageEvaluationReport =
   registeredFunctions.manageEvaluationReport;
+export const requestReportEmailVerification =
+  registeredFunctions.requestReportEmailVerification;
