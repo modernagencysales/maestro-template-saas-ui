ab922492c0ce07886d9939859fb04d9454a7c531
mini-headless <headless@modernagencysales.com>
2026-08-02 17:55:14 -0400

    fix: expose App Idea webhook retry mutation

--- packages/convex/convex/commerce/webhooks.ts
+++ packages/convex/convex/commerce/webhooks.ts
@@ -3,3 +3,4 @@ import registeredFunctions from "../../confect/_generated/registeredFunctions/co
 export const applyDodo = registeredFunctions.applyDodo;
 export const applyVerifiedDodo = registeredFunctions.applyVerifiedDodo;
 export const markAdmaxxerReported = registeredFunctions.markAdmaxxerReported;
+export const markProcessed = registeredFunctions.markProcessed;
