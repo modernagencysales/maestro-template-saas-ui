5474595f06094e402d89cbe22f6ba34c0e12b565
mini-headless <headless@modernagencysales.com>
2026-08-13 01:47:12 -0400

    feat: expose authenticated frontend workspace projections

--- packages/convex/convex/access/members.ts
+++ packages/convex/convex/access/members.ts
@@ -1,5 +1,6 @@
 import registeredFunctions from "../../confect/_generated/registeredFunctions/access/members";
 
 export const changeRole = registeredFunctions.changeRole;
+export const list = registeredFunctions.list;
 export const remove = registeredFunctions.remove;
 export const transferOwnership = registeredFunctions.transferOwnership;
--- packages/convex/convex/auth/workspaces.ts
+++ packages/convex/convex/auth/workspaces.ts
@@ -1,3 +1,5 @@
 import registeredFunctions from "../../confect/_generated/registeredFunctions/auth/workspaces";
 
+export const bySlug = registeredFunctions.bySlug;
 export const list = registeredFunctions.list;
+export const me = registeredFunctions.me;
